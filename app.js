3faa647c36a6d52e1a48395c42ea434acd051477
hnarula <hnarula@catalystone.com>
2022-01-31 14:19:37 +0530

    modified ui

---
+++ app.js
@@ -0,0 +1,12 @@
+const firstName = document.querySelector('#firstName');
+
+const btnSubmit = document.querySelector('#btn-submit');
+btnSubmit.disabled = true;
+
+
+
+
+
+firstName.addEventListener('change',()=>{
+    
+});
