4d1dde7602eb91f86dabcf2a2768664299bdd780
hnarula <hnarula@catalystone.com>
2022-01-31 16:11:36 +0530

    feat: added email slider

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const firstName = document.querySelector('#firstName');
+const slider = document.querySelector('#slide');
+const email = document.querySelector('#email');
+
 
 const btnSubmit = document.querySelector('#btn-submit');
-btnSubmit.disabled = true;
-
-
-
-
+// btnSubmit.disabled = true;
 
 firstName.addEventListener('change',()=>{
     
 });
+
+slider.addEventListener('change',()=>{
+    if(slider.checked){
+        email.disabled = false;
+    }
+    else{
+        email.disabled = true;
+    }
+});
+
+
